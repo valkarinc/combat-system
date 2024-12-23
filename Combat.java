e34924596302ad81a8160aa248f2d74aa6432e4f
valkarin <charlesoneofoneslater@gmail.com>
2024-12-23 06:30:40 -0700

    expanded the interface
    includes handling status effects, crit hit chances, equipment & combat styles, definitions coming later

--- Combat.java
+++ Combat.java
@@ -9,4 +9,14 @@ public interface Combat {
     int getAttackSpeed();
     int getLastAttackTick();
     void setLastAttackTick(int tick);
+    //@todo: add in further combat logic
+    void applyStatusEffect(StatusEffect effect);
+    void removeStatusEffect(StatusEffect effect);
+    boolean hasStatusEffect(StatusEffect effect);
+    int getCriticalHitChance();
+    void equip(Item item);
+    void unequip(Item item);
+    boolean hasEquipped(Item item);
+    CombatStyle getCombatStyle();
+    void setCombatStyle(CombatStyle style);
 }
